5fac568d24236bab13a488397dd0d2022a8e8174
Phil Samuel <philipmayr@outlook.com>
2024-11-06 18:02:05 -0600

    Create program.c

---
+++ program.c
@@ -0,0 +1,36 @@
+// Binary Euclidean Algorithm
+
+#include <stdio.h>
+
+int find_greatest_common_divisor(int a, int b)
+{
+    int g = 1;
+    
+    while ((~a & 1) && (~b & 1))
+    {
+        a >>= 1;
+        b >>= 1;
+        g >>= 1;
+    }
+    
+    while (a)
+    {
+        while (~a & 1) a >>= 1;
+        while (~b & 1) b >>= 1;
+        
+        if (a >= b) a = (a - b) >> 1;
+        else b = (b - a) >> 1;
+    }
+    
+    return g * b;
+}
+
+int main()
+{
+    int a = 37;
+    int b = 73;
+    
+    printf("%d", find_greatest_common_divisor(a, b));
+
+    return 0;
+}
